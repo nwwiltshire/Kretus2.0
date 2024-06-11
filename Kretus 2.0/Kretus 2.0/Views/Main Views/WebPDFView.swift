0221fe2ed317c2e89a0de901f031a1e3887b0191
Nick Wiltshire <nwwiltshire@gmail.com>
2024-06-11 16:04:52 -0700

    Initialized PDF Implementation

--- Kretus 2.0/Kretus 2.0/Views/Main Views/WebPDFView.swift
+++ Kretus 2.0/Kretus 2.0/Views/Main Views/WebPDFView.swift
@@ -9,49 +9,14 @@ import Foundation
 import SwiftUI
 import WebKit
 
-/*
-struct RemotePDFView: View {
-
-  let pdfURLString: String
-
-  var body: some View {
-    ZStack {
-      WKWebView(frame: .zero, configuration: WKWebViewConfiguration()) { webView in
-        guard let url = URL(string: pdfURLString) else { return }
-        webView.load(URLRequest(url: url))
-      }
-      // Add a placeholder view while loading (optional)
-      ProgressView()
-        .opacity(0.5)
-    }
-  }
+struct WebView: UIViewRepresentable {
+ let url: URL
+ func makeUIView(context: Context) -> WKWebView  {
+ let wkwebView = WKWebView()
+ let request = URLRequest(url: url)
+ wkwebView.load(request)
+ return wkwebView
+ }
+ func updateUIView(_ uiView: WKWebView, context: Context) {
+ }
 }
-
-
-class MyViewController: UIViewController {
-
-  private var webView: WKWebView!
-  let pdfURLString = "https://www.example.com/your_pdf.pdf" // Replace with your actual URL
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupWebView()
-    loadPDF()
-  }
-
-  private func setupWebView() {
-    webView = WKWebView(frame: view.frame)
-    view.addSubview(webView)
-  }
-
-  private func loadPDF() {
-    guard let pdfURL = URL(string: pdfURLString) else {
-      // Handle invalid URL error
-      return
-    }
-    let request = URLRequest(url: pdfURL)
-    webView.load(request)
-  }
-}
-*/
-
